f131d9aae005ff8257fefeac5ea91e77665c7a94
Joao Ricardo <joaodsantos.dev@gmail.com>
2026-01-08 20:32:50 -0300

    Implementa herança e polimorfismo no desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone iphone = new Iphone(
+            numero: "49999999999",
+            modelo: "iPhone 14",
+            imei: "123456789",
+            memoria: 128
+        );
+
+        Smartphone nokia = new Nokia(
+            numero: "49888888888",
+            modelo: "Nokia 1",
+            imei: "987654321",
+            memoria: 32
+        );
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
